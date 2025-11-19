69d5a79521260d6fb7c5268a516002d169327721
mustafa-masood <hanisiddiqui123@gmail.com>
2025-11-19 12:22:24 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MoneyMate_Personal_Finance_Tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
